41fbc5a83ce7fa633b1bdcce0181493caac19ecd
Nikitosb888bbb <nikitakuzya2007@gmail.com>
2026-05-14 10:58:06 +0300

    Update 3d_step.py

--- 3d_step.py
+++ 3d_step.py
@@ -1,35 +1,84 @@
-class Weapon:
-    def __init__(self, name):
-        self.name = name
+import random
 
-    def use (self,monster):
-        pass
-
-class SilverSword(Weapon):
-    def __init__(self):
-        super().__init__('Серебрянный меч')
-        self.dmg = 30
-
-    def use(self,monster):
-        print (f'Охотник наносит рассекающий удар {self.name}! {self.dmg} урона.')
-        monster.take_damage(self.dmg)
+class Monster():
+    def __init__(self, name, hp, dmg):
+        self.__name = name
+        self.__hp = hp
+        self.__dmg = dmg
 
 
-class HolyWater(Weapon):
-    def __init__(self):
-        super().__init__('Святая вода')
-        self.dmg = 20
+    def get_name(self):
+        return self.__name
 
-    def use(self,monster):
-        print (f'Охотник обливает врага: {self.name}. {self.dmg} урона.')
-        monster.take_damage(self.dmg)
+    def get_hp(self):
+        return self.__hp
+
+    def get_dmg(self):
+        return self.__dmg
+
+    def set_hp(self, value):
+        if value < 0:
+            self.__hp = 0
+        else:
+            self.__hp = value
+
+    def is_alive(self):
+        return True if self.__hp > 0 else False
+
+    def show_status(self):
+        print (f'{self.__name} HP: {self.__hp}')
+
+    def take_damage(self, damage):
+        self.set_hp(self.__hp - damage)
+
+    def attack_hunter(self, hunter):
+        hunter.set_hp(hunter.get_hp() - self.__dmg)
 
 
-class CrossbowBolt(Weapon):
-    def __init__(self):
-        super().__init__('Арбалет с болтом')
-        self.dmg = 25
+class Zombie(Monster):
+    def __init__(self, name = 'Зомби'):
+        super().__init__(name, 120, 10)
 
-    def use(self,monster):
-        print (f'Охотник делает выстрел: {self.name}! На {self.dmg} урона')
-        monster.take_damage(self.dmg)
+    def take_damage(self, damage):
+        self.set_hp(self.get_hp() - damage)
+        print (f'{self.get_name()} теряет конечность! Получено {damage}. HP: {self.get_hp()}')
+
+
+class Vampire(Monster):
+    def __init__(self, name = 'Вампир'):
+        super().__init__(name,80,15)
+
+    def take_damage(self, damage):
+        feature = max (0, damage - 5)
+        self.set_hp(self.get_hp() - feature)
+        print (f'{self.get_name()} поглащает 5 урона! Получено {feature}. HP: {self.get_hp()}')
+
+
+class Ghost(Monster):
+    def __init__(self, name = 'Призрак'):
+        super().__init__(name,60,20)
+
+    def take_damage(self, damage):
+        if random.random() < 0.3:
+            print (f'{self.get_name()} уклонился от удара! Не прошло {damage}. HP: {self.get_hp()}')
+        else:
+            self.set_hp(self.get_hp() - damage)
+            print (f'{self.get_name()} пропустил двоечку! Получено {damage}. HP: {self.get_hp()}')
+
+
+class Werewolf(Monster):
+    def __init__(self, name = 'Оборотень'):
+        super().__init__(name,100,25)
+        self.__transformed = False
+
+    def take_damage(self,damage):
+        self.set_hp(self.get_hp() - damage)
+        print (f'{self.get_name()} получает в пасть! Получено {damage}. HP: {self.get_hp()}')
+        if self.__hp < 50 and self.__transformed == False:
+            print (f'{self.get_name()} имеет меньше 50 HP. Оборотень трансформируется!')
+            self.__transformed = True
+
+
+
+
+
